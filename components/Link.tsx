a1cdc3bef05d3a6b5fb69e1e3473e42cbf8b9cee
Agustin Kassis <webmaster@masize.com>
2022-11-18 15:55:41 -0300

    link style

--- components/Link.tsx
+++ components/Link.tsx
@@ -5,15 +5,19 @@ interface ILinkProps {
 }
 
 const Link = ({ data }: ILinkProps) => {
+  const { title, type, url } = data;
   return (
     <li>
       <a
-        href={"#"}
-        className='flex font-share justify-center bg-white border-solid border-2 border-slate-50 rounded-md p-2
-        transition duration-300 ease-out
-       hover:bg-slate-100 hover:scale-105'
+        href={url}
+        target='_blank'
+        rel='noreferrer'
+        className='flex font-share justify-center bg-white
+        ring-2 ring-gray-900/5 rounded-md p-2
+        transition duration-400 ease-out
+       hover:bg-slate-50 hover:scale-x-[1.05] hover:scale-y-[1.20]'
       >
-        Link
+        {title}
       </a>
     </li>
   );
