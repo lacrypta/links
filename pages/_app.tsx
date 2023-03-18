af3ccc92ab0d8031a5f26c8332bf5e144d06efa2
Agustin Kassis <webmaster@masize.com>
2023-03-18 04:19:59 -0300

    removed Config dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/fonts.css";
 import "../styles/globals.css";
-import { Config } from "../types/config";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
