cff7a6d3020ef5ecc267483140879231e569a674
Agustin Kassis <webmaster@masize.com>
2023-03-19 00:05:48 -0300

    added container theme background

--- components/content/Container.tsx
+++ components/content/Container.tsx
@@ -1,13 +1,23 @@
-interface IContainerProps {
-  children: React.ReactNode;
-}
+import tw from "tailwind-styled-components";
+import styled from "styled-components";
 
-const Container = ({ children }: IContainerProps) => {
-  return (
-    <div className='relative flex min-h-screen w-screen flex-col justify-center overflow-hidden bg-gray-50 py-12 pt-20 px-6'>
-      {children}
-    </div>
-  );
-};
+const ContainerWithCustomCss = styled.div`
+  ${(props) => `background-color: ${props.theme.background_color}`}
+`;
+
+const Container = tw(ContainerWithCustomCss)`
+  relative
+  min-h-screen
+  w-screen
+  py-12 pt-20 px-6
+
+  flex
+  flex-col
+  justify-center
+  
+  overflow-hidden
+
+  bg-gray-50
+`;
 
 export default Container;
