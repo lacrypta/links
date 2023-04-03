8b558af30453dcf714924de556a79e8980ff19b6
Agustin Kassis <webmaster@masize.com>
2023-04-03 10:42:15 -0300

    added webln support

--- components/header/RoundButton.tsx
+++ components/header/RoundButton.tsx
@@ -1,4 +1,12 @@
+declare global {
+  interface Window {
+    webln: any;
+  }
+}
+
 import Image from "next/image";
+import { useEffect } from "react";
+
 import menuSVG from "../../public/images/menu.svg";
 
 interface RoundButtonProps {
@@ -6,9 +14,33 @@ interface RoundButtonProps {
 }
 
 export const RoundButton = ({ className }: RoundButtonProps) => {
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+  }, []);
+
+  const connect = async () => {
+    try {
+      if (!window.webln.enabled) {
+        await window.webln.enable();
+      }
+
+      const res = await window.webln.lnurl("YOUR_LN_URL");
+      console.dir(res);
+    } catch (err: any) {
+      // Tell the user what went wrong
+      console.dir(err);
+      alert(err.message);
+    }
+  };
+
   return (
     <div className={className}>
-      <button className='rounded-full w-10 h-10 bg-white/10 flex justify-center items-center hover:bg-white/25 transition-colors duration-200 active:bg-black/5'>
+      <button
+        onClick={connect}
+        className='rounded-full w-10 h-10 bg-white/10 flex justify-center items-center hover:bg-white/25 transition-colors duration-150 active:bg-black/5'
+      >
         <Image className='w-6 h-6' src={menuSVG} alt='' />
       </button>
     </div>
