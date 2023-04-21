0e989d512683c00e81ff250900882017297f7065
Agustin Kassis <webmaster@masize.com>
2023-04-21 10:46:48 -0300

    wrapped app with verification provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
+import { VerificationProvider } from "../contexts/Verification";
 import "../styles/fonts.css";
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <VerificationProvider>
+      <Component {...pageProps} />
+    </VerificationProvider>
+  );
 }
