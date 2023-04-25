2e7e92a2192fe35ce069b993907d9411bd109acc
Agustin Kassis <webmaster@masize.com>
2023-04-25 17:21:22 -0300

    use Button component

--- components/verification/steps/InstructionsStep.tsx
+++ components/verification/steps/InstructionsStep.tsx
@@ -8,6 +8,7 @@ import GithubUsernameSetup from "../providers/github/UsernameSetup";
 import LocalUsernameSetup from "../providers/local/UsernameSetup";
 import { useCallback, useState } from "react";
 import { useConfig } from "../../../contexts/Config";
+import Button from "../button";
 
 interface InstructionsStepProps {
   username: string;
@@ -61,25 +62,19 @@ export const InstructionsStep = ({ username, next }: InstructionsStepProps) => {
       </div>
 
       <div className='mt-4 text-center relative'>
-        <button
-          type='button'
-          className='my-4 inline-flex items-center space-x-1 justify-center rounded-md border border-transparent bg-blue-100 px-6 py-4 text-md font-medium text-blue-900/50 hover:bg-blue-200 active:bg-blue-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
+        <Button
+          label={isLoading ? "Verificando..." : "Verificar"}
           onClick={verify}
-          disabled={isLoading}
-        >
-          {isLoading ? (
-            "Verificando..."
-          ) : (
-            <>
+          prefix={
+            isLoading ? (
               <MagnifyingGlassIcon
                 className='text-blue-900/50'
                 width={20}
                 height={20}
               />
-              <span>Verificar</span>
-            </>
-          )}
-        </button>
+            ) : undefined
+          }
+        />
       </div>
     </>
   );
