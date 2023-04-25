ab4883b475de2dec7ced1782cc945b0c75b02eca
Agustin Kassis <webmaster@masize.com>
2023-04-25 16:35:38 -0300

    updated button

--- components/verification/button/index.tsx
+++ components/verification/button/index.tsx
@@ -1,13 +1,19 @@
 interface ButtonProps {
-  onClick: () => void;
+  onClick?: () => void;
   prefix?: React.ReactElement;
+  type?: "submit" | "reset" | "button";
   label: string;
 }
 
-export const Button = ({ label, prefix, onClick }: ButtonProps) => {
+export const Button = ({
+  label,
+  type = "button",
+  prefix,
+  onClick,
+}: ButtonProps) => {
   return (
     <button
-      type='button'
+      type={type}
       className='my-4 inline-flex items-center space-x-1 justify-center rounded-md border border-transparent bg-blue-100 px-6 py-4 text-md font-medium text-blue-900/50 hover:bg-blue-200 active:bg-blue-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
       onClick={onClick}
     >
