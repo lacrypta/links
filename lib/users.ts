475efe8f513c911e97423213643d67ac1d883ee0
Agustin Kassis <webmaster@masize.com>
2023-04-25 16:35:28 -0300

    added createUser for hodl lib

--- lib/users.ts
+++ lib/users.ts
@@ -1,8 +1,54 @@
+import { ProviderType } from "../types/provider";
+import {
+  CreatedUserResponse,
+  ResponseDataType,
+  UserData,
+} from "../types/request";
+
+const API_ENPOINT =
+  process.env.NEXT_PUBLIC_USERS_API_URL || "https://hodl.ar/api/users";
+
 export const getUsers = async () => {
-  const url = process.env.USERS_API_URL || "";
-  return fetch(url)
+  return fetch(`${API_ENPOINT}/users`)
     .then((res) => res.json())
     .then((data) => {
       return data;
     });
+};
+
+export const createUser = async (
+  username: string,
+  provider: ProviderType
+): Promise<UserData> => {
+  const jsonData = JSON.stringify({
+    username,
+    provider,
+  });
+
+  // throw new Error("Not implemented");
+
+  try {
+    const response = await fetch(`${API_ENPOINT}/create`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: jsonData,
+    });
+
+    // Check if the response is successful
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const res: CreatedUserResponse = await response.json();
+
+    if (!res.success) {
+      throw new Error(res.message);
+    }
+
+    return res.data as UserData;
+  } catch (e) {
+    throw new Error((e as Error).message);
+  }
 };
