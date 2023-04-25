27a84ae8554dc4426334b8f8271111b93524bd5d
Agustin Kassis <webmaster@masize.com>
2023-04-25 17:02:37 -0300

    improved wallet verification widget

--- components/verification/widgets/wallet.tsx
+++ components/verification/widgets/wallet.tsx
@@ -1,3 +1,4 @@
+import { useQRCode } from "next-qrcode";
 import { UserData } from "../../../types/request";
 
 interface WalletProps {
@@ -5,10 +6,38 @@ interface WalletProps {
 }
 
 export const Wallet = ({ user }: WalletProps) => {
+  const { Canvas } = useQRCode();
+
   return (
     <div>
-      <div>Datos de la Wallet</div>
-      <div>{JSON.stringify(user)}</div>
+      <div>
+        <div className='flex flex-col'>
+          <a
+            className='my-4 inline-flex items-center space-x-1 justify-center rounded-md border border-transparent bg-blue-100 px-6 py-4 text-md font-medium text-blue-900/50 hover:bg-blue-200 active:bg-blue-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
+            href={user.walletUrl}
+            target='_blank'
+            rel='noreferrer'
+          >
+            Ver Wallet
+          </a>
+        </div>
+
+        <div className='flex flex-col justify-center mt-4 text-2xl'>
+          <h4 className='text-center'>LNHub</h4>
+
+          <div className='flex flex-row justify-center'>
+            <Canvas
+              text={`lndhub://${user.lndhub.login}:${user.lndhub.password}@${user.lndhub.url}`}
+              options={{
+                level: "M",
+                margin: 3,
+                scale: 4,
+                width: 350,
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
