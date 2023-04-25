dfaf7d5dda5ccc0b14bc43f5fa2e6ab8e8b56591
Agustin Kassis <webmaster@masize.com>
2023-04-25 09:37:34 -0300

    added provider type

--- types/provider.ts
+++ types/provider.ts
@@ -1,1 +1,8 @@
-export type ProviderType = "github";
+import { Config } from "./config";
+
+export type ProviderType = "github" | "local";
+
+export interface ConfigProvider {
+  type: ProviderType;
+  get: (username: string) => Promise<Config>;
+}
