3efcbd7c4a9f2ee3fccfda85a28a79ce25e35177
Agustin Kassis <webmaster@masize.com>
2023-04-26 16:19:37 -0300

    added user on config context

--- contexts/Config.tsx
+++ contexts/Config.tsx
@@ -1,11 +1,14 @@
 import React, { useCallback, useState } from "react";
 import { Config } from "../types/config";
 import { ConfigProvider as ConfigProviderType } from "../providers/abstract";
+import { User } from "../types/user";
 
 interface IConfigContext {
   config?: Config;
   provider?: ConfigProviderType;
+  userData?: User;
   setConfig: React.Dispatch<React.SetStateAction<Config | undefined>>;
+  setUserData: React.Dispatch<React.SetStateAction<User | undefined>>;
   refresh: () => Promise<Config | null>;
   setProvider: React.Dispatch<
     React.SetStateAction<ConfigProviderType | undefined>
@@ -16,6 +19,7 @@ const ConfigContext = React.createContext<IConfigContext>({
   setConfig: () => {},
   refresh: () => Promise.resolve(null),
   setProvider: () => {},
+  setUserData: () => {},
 });
 
 interface ConfigProviderProps {
@@ -25,6 +29,7 @@ interface ConfigProviderProps {
 export const ConfigProvider = ({ children }: ConfigProviderProps) => {
   const [config, setConfig] = useState<Config>();
   const [provider, setProvider] = useState<ConfigProviderType>();
+  const [userData, setUserData] = useState<User>();
 
   const refresh = useCallback(async (): Promise<Config | null> => {
     if (!provider) {
@@ -37,7 +42,15 @@ export const ConfigProvider = ({ children }: ConfigProviderProps) => {
 
   return (
     <ConfigContext.Provider
-      value={{ config, provider, setConfig, setProvider, refresh }}
+      value={{
+        config,
+        provider,
+        userData,
+        setConfig,
+        setProvider,
+        refresh,
+        setUserData,
+      }}
     >
       {children}
     </ConfigContext.Provider>
