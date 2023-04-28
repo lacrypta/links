c425c97271bdcfd9bef1a5190a913083ee2617d9
Agustin Kassis <webmaster@masize.com>
2023-04-28 10:44:46 -0300

    added disable attr to button

--- components/verification/button/index.tsx
+++ components/verification/button/index.tsx
@@ -3,12 +3,14 @@ interface ButtonProps {
   prefix?: React.ReactElement;
   type?: "submit" | "reset" | "button";
   label: string;
+  disabled?: boolean;
 }
 
 export const Button = ({
   label,
   type = "button",
   prefix,
+  disabled,
   onClick,
 }: ButtonProps) => {
   return (
@@ -16,6 +18,7 @@ export const Button = ({
       type={type}
       className='my-4 inline-flex items-center space-x-1 justify-center rounded-md border border-transparent bg-blue-100 px-6 py-4 text-md font-medium text-blue-900/50 hover:bg-blue-200 active:bg-blue-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2'
       onClick={onClick}
+      disabled={disabled}
     >
       {prefix}
       <span>{label}</span>
